8fd90e0d583800bf6da55741f8344172a76e9445
Saul <saul.diaz.v@gmail.com>
2018-07-23 11:53:01 -0500

    Seleccion

--- wp-content/plugins/btq-booking/assets/js/btq-booking-admin.js
+++ wp-content/plugins/btq-booking/assets/js/btq-booking-admin.js
@@ -3,4 +3,14 @@
     $(function() {
         $('#btq_booking_color_principal').wpColorPicker();
     });
+    
+    // Formulario de opciones
+    $('input[type=radio][name=btq_booking_service]').change(function() {
+		if (this.value == 'tc') {
+			alert("Travel Click");
+		}
+		else if (this.value == 'iph') {
+			alert("Internet Power Hotel");
+		}
+	});
 })(jQuery);
