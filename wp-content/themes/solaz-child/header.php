f265229c1d7c797c8fbff582e3e3c8868d47efc4
Saul <saul.diaz.v@gmail.com>
2018-07-26 18:31:56 -0500

    Google Tag Manager

--- wp-content/themes/solaz-child/header.php
+++ wp-content/themes/solaz-child/header.php
@@ -1,53 +1,51 @@
 <?php 
-$solaz_settings = solaz_check_theme_options(); 
+$solaz_settings = solaz_check_theme_options();
 
-if ( defined( 'ICL_LANGUAGE_CODE' ) ) {
-	$idioma = ICL_LANGUAGE_CODE;
+// Determina el idioma en uso
+$wpml_current_language = apply_filters( 'wpml_current_language', NULL );
+if (!empty($wpml_current_language)){
+	$language = $wpml_current_language;
+}
+elseif ( defined( 'ICL_LANGUAGE_CODE' ) ) {
+	$language = ICL_LANGUAGE_CODE;
 }
 else {
-	$idioma = 'es';
+	$language = 'es';
 }
 
-if ($idioma == 'es'){
-	$google_tag_manager_head = "
-	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-W8P8X4');</script>
-	<!-- End Google Tag Manager -->
-	";
-	
-	$google_tag_manager_body = '
-	<!-- Google Tag Manager (noscript) -->
-	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W8P8X4"
-	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-	<!-- End Google Tag Manager (noscript) -->
-	';
+// De acuerdo al idioma establece el Container ID de Google Tag Manager
+if($language == 'es'){
+	// Español
+	$GoogleTagManagerContainerID = 'GTM-W8P8X4';
 }
-else {
-	$google_tag_manager_head = "
-	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-PK97P2');</script>
-	<!-- End Google Tag Manager -->
-	";
-	
-	$google_tag_manager_body = '
-	<!-- Google Tag Manager (noscript) -->
-	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PK97P2"
-	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-	<!-- End Google Tag Manager (noscript) -->
-	';
+else{
+	// Inglés
+	$GoogleTagManagerContainerID = 'GTM-PK97P2';
 }
+
+// Establece el Google Tag Manager del header
+$GoogleTagManagerHead = "
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','" . $GoogleTagManagerContainerID . "');</script>
+<!-- End Google Tag Manager -->
+";
+
+// Establece el Google Tag Manager del body
+$GoogleTagManagerBody = '
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=' . $GoogleTagManagerContainerID . '
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+';
+
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<?php echo $google_tag_manager_head; ?>
+	<?php echo $GoogleTagManagerHead; ?>
     <meta charset="<?php bloginfo('charset'); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11" />
@@ -76,7 +74,7 @@ if (is_404() ) {
 }
 ?>
 <body <?php body_class(); ?>>
-	<?php echo $google_tag_manager_body; ?>
+	<?php echo $GoogleTagManagerBody; ?>
     <?php echo solaz_pre_loader();?>
 	<div id="page" class="hfeed site <?php if(!$solaz_remove_space){echo 'remove_space';}?> 
                                      <?php if(!$solaz_remove_space_br){echo 'remove_space_br';}?> 
